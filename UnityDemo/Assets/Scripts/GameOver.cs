f1df03f6b7a2150752eb04f380fa050136173f36
realdcoutinho <diogo.de.moura.coutinho@student.howest.be>
2022-10-16 21:41:50 +0200

    update

--- UnityDemo/Assets/Scripts/GameOver.cs
+++ UnityDemo/Assets/Scripts/GameOver.cs
@@ -1,51 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-using UnityEngine.UI;
-public class HUD : MonoBehaviour
+using UnityEngine.SceneManagement;
+public class GameOver : MonoBehaviour
 {
-    //include the UnityEngine.UI namespace
-    [SerializeField] Image _healthBar = null;
-    [SerializeField] Text _primaryAmmo = null;
-    [SerializeField] Text _secondaryAmmo = null;
+    [SerializeField]
+    private GameObject _player = null;
 
-    private Health _playerHealth = null;
-    private ShootingBehaviour _playerShootingBehaviour = null;
-
-
-    private void Start()
+    void Update()
     {
-        PlayerCharacter player = FindObjectOfType<PlayerCharacter>();
-
-        if (player != null)
-        {
-            _playerHealth = player.GetComponent<Health>();
-            _playerShootingBehaviour = player.GetComponent<ShootingBehaviour>();
-        }
+        if (_player == null)
+            TriggerGameOver();
     }
 
-    private void Update()
+    void TriggerGameOver()
     {
-        SyncData();
-    }
-
-    void SyncData()
-    {
-        //health
-        if (_healthBar && _playerHealth)
-        {
-            _healthBar.transform.localScale = new Vector3(_playerHealth.HealthPercentage, 1.0f, 1.0f);
-        }
-
-        //ammo
-        if (_primaryAmmo && _playerShootingBehaviour)
-        {
-            _primaryAmmo.text = _playerShootingBehaviour.PrimaryWeaponAmmo.ToString();
-        }
-        if (_secondaryAmmo && _playerShootingBehaviour)
-        {
-            _secondaryAmmo.text = _playerShootingBehaviour.SecondaryWeaponAmmo.ToString();
-        }
+        //include the namespace UnityEngine.SceneManagmenet
+        SceneManager.LoadScene(0);
     }
 }
